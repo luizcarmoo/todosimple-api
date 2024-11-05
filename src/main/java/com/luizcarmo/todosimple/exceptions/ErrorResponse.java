89dad44ee0d0a8df96d6aea82378d0d08732a137
Luiz Carmo <luiz.pcarmo@gmail.com>
2024-11-05 02:35:36 -0300

    Create JWTAuthenticationFilter

--- src/main/java/com/luizcarmo/todosimple/exceptions/ErrorResponse.java
+++ src/main/java/com/luizcarmo/todosimple/exceptions/ErrorResponse.java
@@ -32,4 +32,8 @@ public class ErrorResponse {
     this.errors.add(new ValidationError(field, message));
   }
 
+  public String toJson() {
+    return "{\"status\": " + getStatus() + ", " +
+      "\"message\": \"" + getMessage() + "\"}";
+    }
 }

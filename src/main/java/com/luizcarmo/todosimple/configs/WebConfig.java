c092a3f46831fd69ea44f175081527d7bdd570eb
Luiz Carmo <luiz.pcarmo@gmail.com>
2024-11-14 15:02:18 -0300

    Frontend refactor

--- src/main/java/com/luizcarmo/todosimple/configs/WebConfig.java
+++ src/main/java/com/luizcarmo/todosimple/configs/WebConfig.java
@@ -8,14 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-  
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowedHeaders("*")
-            .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS")                .allowedOrigins("*")
-            .allowedHeaders("*")
-            .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
-  }
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+    }
 
 }
--- webContent/scripts/script.js
+++ webContent/scripts/script.js
@@ -1,45 +1,109 @@
-const tasksEndpoint = "http://localhost:8080/task/user";
-
-function hideLoader() {
-  document.getElementById("loading").style.display = "none";
+function logout() {
+  localStorage.removeItem('Authorization');
+  window.location = '../login_page/login.html';
 }
 
-function show(tasks) {
-  let tab = `<thead>
-            <th scope="col">#</th>
-            <th scope="col">Description</th>
-        </thead>`;
+document.addEventListener("DOMContentLoaded", function () {
+  const tasksEndpoint = "http://localhost:8080/task/user";
 
-  for (let task of tasks) {
-    tab += `
-            <tr>
-                <td scope="row">${task.id}</td>
-                <td>${task.description}</td>
-            </tr>
-        `;
+  function hideLoader() {
+    document.getElementById("loading").style.display = "none";
   }
 
-  document.getElementById("tasks").innerHTML = tab;
-}
+  function show(tasks) {
+    let tab = `<thead>
+              <th scope="col">#</th>
+              <th scope="col">Description</th>
+          </thead>`;
 
-async function getTasks() {
-  let key = "Authorization";
-  const response = await fetch(tasksEndpoint, {
-    method: "GET",
-    headers: new Headers({
-      Authorization: localStorage.getItem(key),
-    }),
+    for (let task of tasks) {
+      tab += `
+              <tr id="task-row-${task.id}">
+                  <td scope="row">${task.id}</td>
+                  <td>${task.description}</td>
+              </tr>
+          `;
+    }
+
+    document.getElementById("tasks").innerHTML = tab;
+  }
+
+  async function getTasks() {
+    let key = "Authorization";
+    const response = await fetch(tasksEndpoint, {
+      method: "GET",
+      headers: new Headers({
+        Authorization: localStorage.getItem(key),
+      }),
+    });
+
+    var data = await response.json();
+    console.log(data);
+    if (response) hideLoader();
+    show(data);
+    deleteButton(data);
+  }
+
+  function deleteButton(tasks) {
+    tasks.forEach(task => {
+      const deleteButton = document.createElement("button");
+      deleteButton.className = "btn btn-danger btn-sm";
+      deleteButton.innerHTML = "&#x2715;";
+      deleteButton.onclick = function() { deleteTask(task.id); };
+      
+      const taskRow = document.getElementById(`task-row-${task.id}`);
+      const actionCell = document.createElement("td");
+      actionCell.appendChild(deleteButton);
+      taskRow.appendChild(actionCell);
+    });
+  }
+
+  async function deleteTask(taskId) {
+    let key = "Authorization";
+    
+    const response = await fetch(`http://localhost:8080/task/${taskId}`, {
+      method: "DELETE",
+      headers: new Headers({
+        Authorization: localStorage.getItem(key),
+      }),
+    });
+
+    if (response.ok) {
+      getTasks(); 
+    } else {
+      alert("Error deleting task.");
+    }
+  }
+
+  document.getElementById("taskForm").addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    let key = "Authorization";
+    const taskDescription = document.getElementById("taskDescription").value;
+
+    const response = await fetch("http://localhost:8080/task", {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json; charset=utf8",
+        Accept: "application/json",
+        Authorization: localStorage.getItem(key),
+      }),
+      body: JSON.stringify({
+        description: taskDescription,
+      }),
+    });
+
+    if (response.ok) {
+      let taskModal = bootstrap.Modal.getInstance(document.getElementById("taskModal"));
+      taskModal.hide();
+      getTasks(); 
+    } else {
+      alert("Error adding task.");
+    }
   });
 
-  var data = await response.json();
-  console.log(data);
-  if (response) hideLoader();
-  show(data);
-}
-
-document.addEventListener("DOMContentLoaded", function (event) {
   if (!localStorage.getItem("Authorization"))
     window.location = "/webContent/home_page/index.html";
-});
 
-getTasks();
+  getTasks();
+});
